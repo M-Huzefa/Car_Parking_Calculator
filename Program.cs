fe6230bd9c3531bf4a91455b60da012d0211b69c
Muhammad Huzefa <105141452+M-Huzefa@users.noreply.github.com>
2022-07-04 01:02:34 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿class Vehicle
+{
+    protected double initial;
+    protected double increment;
+    protected double maximum;
+    protected double time = 0;
+
+    public void Cal(double time)
+    {
+        double total;
+        if (time <= 3)
+        {
+            Console.WriteLine("Fare for {0} is ${1}", time, initial);
+        }
+        if (time >= 3)
+        {
+            total = initial + increment * (time - 3);
+            if (total > maximum)
+            {
+                Console.WriteLine("Fare for {0} is ${1}", time, maximum);
+            }
+            else
+            {
+                Console.WriteLine("Fare for {0} is ${1}", time, total);
+            }
+        }
+    }
+}
+
+class Bus : Vehicle
+{
+    public Bus(double ini, double incre, double max)
+    {
+        initial = ini;
+        increment = incre;
+        maximum = max;
+    }
+}
+
+class Car : Vehicle
+{
+    public Car(double ini, double incre, double max)
+    {
+        initial = ini;
+        increment = incre;
+        maximum = max;
+    }
+}
+
+class Bike : Vehicle
+{
+    public Bike(double ini, double incre, double max)
+    {
+        initial = ini;
+        increment = incre;
+        maximum = max;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle bus = new Bus(6, 1.5, 30);
+        Vehicle car = new Car(4, 1, 20);
+        Vehicle bike = new Bike(2, 0.5, 10);
+        Console.WriteLine("In case of Bus: ");
+        bus.Cal(24);
+        bus.Cal(5);
+        bus.Cal(1);
+        Console.WriteLine("In case of Car: ");
+        car.Cal(24);
+        car.Cal(5);
+        car.Cal(1);
+        Console.WriteLine("In case of Bike: ");
+        bike.Cal(24);
+        bike.Cal(5);
+        bike.Cal(1);
+    }
+}
