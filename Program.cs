9d813a121d1cdf96e24b5d7cf712b9f29f3733d3
Muhammad Huzefa <105141452+M-Huzefa@users.noreply.github.com>
2022-07-04 00:43:19 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,81 +1,86 @@
-﻿class Vehicle
-{
-    protected double initial;
-    protected double increment;
-    protected double maximum;
-    protected double time = 0;
+﻿using System;
 
-    public void Cal(double time)
+namespace car_parking_calculator
+{
+    class Vehicle
     {
-        double total;
-        if (time <= 3)
+        protected double initial;
+        protected double increment;
+        protected double maximum;
+        protected double time = 0;
+
+        public void Calculator (double time)
         {
-            Console.WriteLine("Fare for {0} is ${1}", time, initial);
+            double total;
+            if (time <= 3)
+            {
+                Console.WriteLine("Fare for {0} hours is ${1}", time, initial);
+            }
+            if (time >= 3)
+            {
+                total = initial + increment * (time - 3);
+                if (total > maximum)
+                {
+                    Console.WriteLine("Fare for {0} hours is ${1}", time, maximum);
+                }
+                else
+                {
+                    Console.WriteLine("Fare for {0} hours is ${1}", time, total);
+                }
+            }
         }
-        if (time >= 3)
+    }
+
+    class Bus : Vehicle
+    {
+        public Bus()
         {
-            total = initial + increment * (time - 3);
-            if (total > maximum)
-            {
-                Console.WriteLine("Fare for {0} is ${1}", time, maximum);
-            }
-            else
-            {
-                Console.WriteLine("Fare for {0} is ${1}", time, total);
-            }
+            initial = 6;
+            increment = 1.5;
+            maximum = 30;
         }
     }
-}
 
-class Bus : Vehicle
-{
-    public Bus()
+    class Car : Vehicle
     {
-        initial = 6;
-        increment = 1.5;
-        maximum = 30;
+        public Car()
+        {
+            initial = 4;
+            increment = 1;
+            maximum = 20;
+        }
     }
-}
 
-class Car : Vehicle
-{
-    public Car()
+    class Bike : Vehicle
     {
-        initial = 4;
-        increment = 1;
-        maximum = 20;
+        public Bike()
+        {
+            initial = 2;
+            increment = 0.5;
+            maximum = 10;
+        }
     }
-}
 
-class Bike : Vehicle
-{
-    public Bike()
+
+    class Program
     {
-        initial = 2;
-        increment = 0.5;
-        maximum = 10;
-    }
-}
-
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Vehicle bus = new Bus();
-        Vehicle car = new Car();
-        Vehicle bike = new Bike();
-        Console.WriteLine("In case of Bus: ");
-        bus.Cal(24);
-        bus.Cal(5);
-        bus.Cal(1);
-        Console.WriteLine("In case of Car: ");
-        car.Cal(24);
-        car.Cal(5);
-        car.Cal(1);
-        Console.WriteLine("In case of Bike: ");
-        bike.Cal(24);
-        bike.Cal(5);
-        bike.Cal(1);
+        static void Main(string[] args)
+        {
+            Vehicle bus = new Bus();
+            Vehicle car = new Car();
+            Vehicle bike = new Bike();
+            Console.WriteLine("In case of Bus: ");
+            bus.Calculator(24);
+            bus.Calculator(5);
+            bus.Calculator(1);
+            Console.WriteLine(); Console.WriteLine("In case of Car: ");
+            car.Calculator(24);
+            car.Calculator(5);
+            car.Calculator(1);
+            Console.WriteLine(); Console.WriteLine("In case of Bike: ");
+            bike.Calculator(24);
+            bike.Calculator(5);
+            bike.Calculator(1);
+        }
     }
 }
